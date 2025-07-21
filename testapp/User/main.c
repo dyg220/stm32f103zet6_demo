0fdcae7a84d4665fdfdcc0f713d065cd8aaa2584
dyg220 <1503639251@qq.com>
2025-07-21 17:39:14 +0800

    add

--- testapp/User/main.c
+++ testapp/User/main.c
@@ -1,14 +1,26 @@
 #include "main.h"
 
+void delay(int xms)
+{
+	int i = SystemCoreClock / 1000;
+	int j = 0;
+	while (xms--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			__NOP();
+		}
+	}
+}
 int main()
 {
 	LED_Init();
 	while (1)
 	{
 		LED3_ON;
-		LED4_ON;
-		LED5_ON;
-		LED6_ON;
+		delay(10);
+		LED3_OFF;
+		delay(10);
 	}
 	return 0;
 }
