9e0e3b08ee9aef866b4074424b38b9259b9e5939
dyg220 <1503639251@qq.com>
2025-07-26 21:56:56 +0800

    add

--- testapp/Hardwear/MAX7219/MAX7219.c
+++ testapp/Hardwear/MAX7219/MAX7219.c
@@ -1,7 +1,17 @@
 #include "MAX7219.h"
 
+//blog.csdn.net/weichen_78/article/details/128284559
 void MAX7219_Init(void)
 {
+	//PE1 PE3 PE5
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_3 | GPIO_Pin_5;
+	GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+
 
 
 }
--- testapp/Hardwear/MAX7219/MAX7219.h
+++ testapp/Hardwear/MAX7219/MAX7219.h
@@ -2,6 +2,13 @@
 #define _MAX7219_H_
 #include "stm32f10x.h"
 
+#define MAX7219_CS_H	GPIO_SetBits(GPIOE, GPIO_Pin_1)
+#define MAX7219_CS_L	GPIO_ResetBits(GPIOE, GPIO_Pin_1)
+
+
 void MAX7219_Init(void);
+
+
+
 #endif // !_MAX7219_H_
 
