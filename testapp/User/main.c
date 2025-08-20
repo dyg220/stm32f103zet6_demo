7671930923971b8625786de440ba0cd6f142d20f
dyg220 <1503639251@qq.com>
2025-08-20 10:05:02 +0800

    add

--- testapp/User/main.c
+++ testapp/User/main.c
@@ -3,13 +3,14 @@
 
 int main()
 {
-    LED_Init();
-    Usart1_Init(115200);
+	LED_Init();
+	Usart1_Init(115200);
 	MAX7219_Init();
-    while (1)
-    {
-        printf("test2222-------\r\n");
+	while (1)
+	{
+		printf("test2222-------\r\n");
+		Delay_ms(500);
+	}
 
-    }
-    return 0;
+	return 0;
 }
--- testapp/User/main.h
+++ testapp/User/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 #include "led.h"
+#include "delay.h"
 #include "usart1.h"
 #include "max7219.h"
 
